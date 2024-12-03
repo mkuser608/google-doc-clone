39506a702a31a61cabf30e310b9256320bef0b2f
M K Mandal <mkuser608@gmail.com>
2024-12-04 05:28:12 +0530

    ruler added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,11 +21,13 @@ import { FontSizeExtension } from "@/extensions/font-size";
 import { LineHeightExtension } from "@/extensions/line-height";
 
 import { useEditorStore } from "@/store/use-editor-store";
+import { Ruler } from "./ruler";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -54,16 +56,13 @@ export const Editor = () => {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px;",
         class:
-          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] fles flex-col min-h-[1054px] w-[816px] pt-10 pb-10 cursor-text",
+          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pb-10 cursor-text",
       },
     },
     extensions: [
       StarterKit,
       FontSizeExtension,
-      LineHeightExtension.configure({
-        types: ["heading", "paragraph"],
-        defaultLineHeight: "normal",
-      }),
+      LineHeightExtension,
       Table,
       TableCell,
       TableHeader,
@@ -111,6 +110,7 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD} px-4 print:bg-white print:p-0 print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center  w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
