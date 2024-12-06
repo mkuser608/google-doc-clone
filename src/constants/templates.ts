182f5489d4521bc9b1663fc0e1325e94648dbb7f
M K Mandal <mkuser608@gmail.com>
2024-12-06 05:59:24 +0530

    template and convex db added

---
+++ src/constants/templates.ts
@@ -0,0 +1,26 @@
+export const templates = [
+  { id: "blank", label: "Balnk Document", imageUrl: "./blank-document.svg" },
+  {
+    id: "software-proposal",
+    label: "Software Developement Proposal",
+    imageUrl: "./software-proposal.svg",
+  },
+  {
+    id: "project-proposal",
+    label: "Project proposal",
+    imageUrl: "./project-proposal.svg",
+  },
+  {
+    id: "business-letter",
+    label: "Buisness Letter",
+    imageUrl: "./business-letter.svg",
+  },
+
+  { id: "resume", label: "Resume", imageUrl: "./resume.svg" },
+  {
+    id: "cover-letter",
+    label: "Cover Letter",
+    imageUrl: "./cover-letter.svg",
+  },
+  { id: "letter", label: "Letter", imageUrl: "./letter.svg" },
+];
